c5143ec7cad0af03f8776e1ebe5b562e3f7bb18d
gaelazzo <gaetano.lazzo@gmail.com>
2018-07-04 17:17:54 +0200

    serializzo un paio di ext.prop in table

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function (grunt) {
                     url: '<%= pkg.homepage %>',
                     options: {
                         paths: ['./src'],
-                        outdir: 'doc'
+                        outdir: 'docs'
                     }
                 }
             },
---
+++ docs/api.js
@@ -0,0 +1,31 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "AutoIncrementColumn",
+        "DataRelation",
+        "DataRow",
+        "DataRowObserver",
+        "DataSet",
+        "DataTable",
+        "ObjectRow",
+        "OptimisticLocking"
+    ],
+    "modules": [
+        "DataRow",
+        "DataSet"
+    ],
+    "allModules": [
+        {
+            "displayName": "DataRow",
+            "name": "DataRow",
+            "description": "DataRow shim, provides methods to manage objects as Ado.Net DataRows"
+        },
+        {
+            "displayName": "DataSet",
+            "name": "DataSet",
+            "description": "Provides shim for Ado.net DataSet class"
+        }
+    ],
+    "elements": []
+} };
+});
