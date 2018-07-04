5c77a17c52e2acbe12560b54b1e180f211c76695
unknown <fabioxsalvi@gmail.com>
2018-07-04 16:21:46 +0200

    Added yuidoc in /docs

---
+++ docs/api.js
@@ -0,0 +1,33 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "$rowState",
+        "$rowVersion",
+        "AutoIncrementColumn",
+        "DataRelation",
+        "DataRow",
+        "DataRowObserver",
+        "DataSet",
+        "DataTable",
+        "ObjectRow",
+        "OptimisticLocking"
+    ],
+    "modules": [
+        "DataRow",
+        "DataSet"
+    ],
+    "allModules": [
+        {
+            "displayName": "DataRow",
+            "name": "DataRow",
+            "description": "DataRow shim, provides methods to manage objects as Ado.Net DataRows"
+        },
+        {
+            "displayName": "DataSet",
+            "name": "DataSet",
+            "description": "Provides shim for Ado.net DataSet class"
+        }
+    ],
+    "elements": []
+} };
+});
