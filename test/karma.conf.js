d5c158ffdd8cc940720620482c308b65c4a8f04b
gaelazzo <gaetano.lazzo@gmail.com>
2021-02-24 10:55:37 +0100

    update node version

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -54,20 +54,25 @@ module.exports = function(config) {
         // Which plugins to enable
         plugins: [
             //'karma-phantomjs-launcher',
-            'karma-chrome-launcher',
-            'karma-jasmine'
-            //'karma-junit-reporter'
+            'karma-jasmine', 'karma-chrome-launcher','karma-jasmine-html-reporter','karma-spec-reporter'
         ],
 
         // Continuous Integration mode
         // if true, it capture browsers, run tests and exit
         //singleRun: false,
+        client: {
+            jasmine: {
+                random:false,
+                failFast: false,
+                timeoutInterval: 5000
+            }
+        },
 
         colors: true,
 
         // level of logging
         // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-        logLevel: config.LOG_INFO
+        logLevel: config.LOG_DEBUG
 
         // Uncomment the following lines if you are using grunt's server to run the tests
         // proxies: {
